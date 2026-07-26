965285c39c54bbacc1d9d0d368886845b5e12087
Niclas Helbro <niclas.helbro@gmail.com>
2026-07-26 20:36:36 +0200

    Glyph fixes

--- blocks.def.h
+++ blocks.def.h
@@ -7,12 +7,13 @@ static const Block blocks[] = {
 	{"󰍛 ",      "$HOME/.config/dwmblocks/memory.sh",                10,                 0},
 	{" ",      "$HOME/.config/dwmblocks/disk.sh",                  60,                 0},
 	{" ",      "$HOME/.config/dwmblocks/cpu-temperature.sh",		60,                 0},
+	{" ",      "$HOME/.config/dwmblocks/brightness.sh",            0,                  11},
 	{"󰂀 ",      "$HOME/.config/dwmblocks/battery.sh",               60,                 0},
 	{"󰕾 ",      "$HOME/.config/dwmblocks/volume.sh",                0,                  10},
-	{" ",      "$HOME/.config/dwmblocks/brightness.sh",            0,                  11},
-	{"",		"$HOME/.config/dwmblocks/date.sh",					60,                 0},
-	{" ",      "whoami",                                           0,                  0},
-	{"",        "$HOME/.config/dwmblocks/reboot-required.sh",		5,                  0},
+	{" ",		"$HOME/.config/dwmblocks/date.sh",					60,                 0},
+	{" ",		"$HOME/.config/dwmblocks/time.sh",					60,                 0},
+	// {" ",      "whoami",                                           0,                  0},
+	// {"",        "$HOME/.config/dwmblocks/reboot-required.sh",		5,                  0},
 	//{"󰍛 ",    "$HOME/.config/dwm/scripts/dwmblocks-mem.sh",               30,                 0},
 	//{" :",   "$HOME/.config/dwm/scripts/dwmblocks-disk.sh",              30,                 0},
 	//{"󰂀 ",    "$HOME/.config/dwm/scripts/dwmblocks-bat.sh",               30,                 0},
@@ -21,5 +22,5 @@ static const Block blocks[] = {
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " | ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
